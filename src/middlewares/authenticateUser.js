02ba0d7d66bb7e4a8e0eada96473a006fcdab00d
Suraj Verma <surajverma@rentalcars.com>
2020-09-23 16:49:01 +0100

    add auth middleware and protect api routes with login and register support

---
+++ src/middlewares/authenticateUser.js
@@ -0,0 +1,40 @@
+const jwt = require("jsonwebtoken");
+
+const db = require("../models");
+const { AUTH_SECRET } = require("../config");
+
+const authenticateUser = async (req, res, next) => {
+  try {
+    const authorizationHeader = req.headers.authorization;
+    const userAuthToken = authorizationHeader.split(" ")[1];
+    const jwtPayload = jwt.verify(userAuthToken, AUTH_SECRET);
+    const user = await db.User.findById(jwtPayload.id);
+
+    if (!user) {
+      res.status(404).send({
+        success: false,
+        message: "User could not be found",
+      });
+
+      return next(new Error(["User could not be found"]));
+    }
+
+    req.user = {
+      id: user._id,
+      email: user.email,
+      firstName: user.firstName,
+      lastName: user.lastName,
+    };
+
+    return next();
+  } catch (error) {
+    res.status(401).send({
+      success: false,
+      message: "You're not authorised to access this route",
+    });
+
+    return next(new Error([error.message]));
+  }
+};
+
+module.exports = authenticateUser;
---
+++ src/models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const schema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+
+  lastName: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", schema);
+
+module.exports = User;
